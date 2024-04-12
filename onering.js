09f9ef9309eca08fd6fb3def9dca5b50dd29c81a
siquu <186956095+siquu@users.noreply.github.com>
2024-04-12 13:22:03 -0400

    add custom element

---
+++ onering.js
@@ -0,0 +1,28 @@
+window.members = [
+  {
+    name: "siqu",
+    url: "https://siqu.neocities.org/",
+    description: "",
+  },
+]
+
+customElements.define(
+  "one-ring",
+  class extends HTMLElement {
+    constructor() {
+      super().attachShadow({ mode: "open" })
+    }
+    connectedCallback() {
+      let members = window.members
+      let currentIndex = members.findIndex((member) => window.location.href.includes(member.url))
+      this.shadowRoot.innerHTML =
+        currentIndex === -1
+          ? `<b>You aren't a part of the One Webring (yet).</b>`
+          : `<div>
+        <a href="${members.at(currentIndex - 1).url}">&larr;</a> <a href="${
+              members.at((currentIndex + 1) % members.length).url
+            }">&rarr;</a>
+        </div>`
+    }
+  }
+)
