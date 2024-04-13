39137b8e7b9474471074ce39226bcb68ae87e76b
siquu <186956095+siquu@users.noreply.github.com>
2024-04-13 11:09:14 -0400

    add suboptimalism, update description

--- onering.js
+++ onering.js
@@ -2,12 +2,17 @@ window.members = [
   {
     name: "siqu",
     url: "https://siqu.neocities.org/",
-    description: "",
+    description: "how useful it is to be useless",
   },
   {
     name: "oiwa",
     url: "https://oiwa.neocities.org/",
     description: "tramontane",
+  },
+  {
+    name: "suboptimalism",
+    url: "https://suboptimalism.neocities.org/",
+    description: "incomplete perfection; complete imperfection",
   },
 ]
 
